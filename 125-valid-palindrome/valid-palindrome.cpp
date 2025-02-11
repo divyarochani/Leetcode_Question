43b1997410501710d2a5cf2ed693aed98abe57d7
Divya Rochani <119523148+divyarochani@users.noreply.github.com>
2025-02-11 22:27:04 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (6.33%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+         
+        string t="";
+        int n=s.size();
+        
+        for(char x:s){
+            
+            if(isalpha(x) || isdigit(x))(t+=tolower(x));
+        }
+        
+        string g=t;
+        reverse(g.begin(),g.end());
+        
+        return t==g;
+    }
+};
