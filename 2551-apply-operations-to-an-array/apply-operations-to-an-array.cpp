8ae722ffd4f45f36027e118ef161f5ebb70fdb37
Divya Rochani <119523148+divyarochani@users.noreply.github.com>
2025-03-09 21:34:59 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (23.38%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]==nums[i+1]){
+        //Apply operations and maintain non-zero operations on the left side
+        int n=nums.size();
+        int non_zero_idx=0;
+
+        for(int i=0;i<n;++i){
+            if(i<n-1 and nums[i]==nums[i+1]){
                 nums[i]*=2;
                 nums[i+1]=0;
             }
+            if(nums[i]!=0)
+                swap(nums[i],nums[non_zero_idx++]);
         }
-        vector<int> ans(nums.size(),0);
-
-        int j=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]!=0){
-                ans[j++]=nums[i];
-            }
-        }
-        return ans;
+        return nums;
     }
 };
