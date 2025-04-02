d5ad31f5accda6ec7583ed51d3110b43cb04529c
Divya Rochani <119523148+divyarochani@users.noreply.github.com>
2025-04-02 21:41:50 +0530

    Time: 158 ms (30.76%) | Memory: 92 MB (22.00%) - LeetSync

---
+++ 2759-flatten-deeply-nested-array/flatten-deeply-nested-array.js
@@ -0,0 +1,16 @@
+var flat = function (arr, n) {
+    if(n === 0){
+        return arr.slice()
+    }
+     let flatarr = []
+
+for (let i = 0; i < arr.length; i++) {
+   if (Array.isArray(arr[i])) {
+      const nested = flat(arr[i], n - 1)
+      flatarr.push(...nested) 
+    } else {
+        flatarr.push(arr[i])
+    }
+  }
+     return flatarr
+};
